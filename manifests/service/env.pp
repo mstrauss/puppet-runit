afc4b16bf5e4a2db3de04f13050556166b1a97fd
Markus Strauss <Markus@ITstrauss.eu>
2012-06-02 04:05:55 +0200

    do not create sv/env when resource is set absent

--- manifests/service/env.pp
+++ manifests/service/env.pp
@@ -4,7 +4,7 @@ define runit::service::env( $service, $value, $ensure = present ) {
   $envdir = "/etc/sv/${service}/env"
   
   # create this directory if at least one environment variable is defined
-  if !defined( File["/etc/sv/${service}/env"] ) {
+  if $ensure == present and !defined( File["/etc/sv/${service}/env"] ) {
     file{ "/etc/sv/${service}/env":
       ensure  => directory,
       # all unmanaged envs. will be removed
