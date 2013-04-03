6ea3991258e8a38b3b1c075ecb8fd8b2b8387f6d
Markus Strauss <Markus@ITstrauss.eu>
2013-04-03 12:14:43 +0200

    env: support env. values with same name on multiple services

--- manifests/service/env.pp
+++ manifests/service/env.pp
@@ -1,5 +1,26 @@
-# the env $name must be unique
-define runit::service::env( $service, $value, $ensure = present ) {
+# Define runit::service::env
+#
+# Defines environment variables for runit services.
+#
+# == Parameters
+#
+#   [*namevar/title*]
+#     A unique name.
+#   [*service*]
+#     The name of the runit service for which this variable is to be defined.
+#   [*envname*]
+#     The name of the environment variable.  Defaults to $title.
+#   [*value*]
+#     The value of the environment variable.
+#
+# == Examples
+#
+#
+# == Requires
+#
+#   runit::service { $service: }
+#
+define runit::service::env( $service, $envname = $title, $value, $ensure = present ) {
 
   $envdir = "/etc/sv/${service}/env"
 
@@ -13,7 +34,7 @@ define runit::service::env( $service, $value, $ensure = present ) {
     }
   }
 
-  file { "${envdir}/${name}":
+  file { "${envdir}/${envname}":
     ensure => $ensure,
     content => "${value}\n",
   }
